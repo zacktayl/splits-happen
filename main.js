42944970394faa5ac62742fc519171ed6a72baf7
Allison Taylor <allisontaylor@Allisons-MacBook-Air.local>
2016-05-11 18:08:56 -0400

    Change from tabs to spaces for better presentation on github

--- main.js
+++ main.js
@@ -1,137 +1,137 @@
 function bowlingGame() {
-	var bowling = {};
+  var bowling = {};
 
-	function frame(a, b) {
-		var _a, _b, _lookahead;
-		var frameObj = {};
+  function frame(a, b) {
+    var _a, _b, _lookahead;
+    var frameObj = {};
 
-		function frameConstructor() {
-			_lookahead = 0;
-			frameObj.setA(a);
-			frameObj.setB(b);
-		}
+    function frameConstructor() {
+      _lookahead = 0;
+      frameObj.setA(a);
+      frameObj.setB(b);
+    }
 
-		frameObj.getA = function() { return _a; };
-		
-		frameObj.setA = function(a) { 
-			if(a == 'X') {
-				_lookahead = 2;
-				_a = 10;
-			} else if(a == '-') {
-				_a = 0;
-			} else if(a == undefined) {
-				_a = a;
-			} else {
-				_a = parseInt(a, 10);
-			}
-		};
+    frameObj.getA = function() { return _a; };
+    
+    frameObj.setA = function(a) { 
+      if(a == 'X') {
+        _lookahead = 2;
+        _a = 10;
+      } else if(a == '-') {
+        _a = 0;
+      } else if(a == undefined) {
+        _a = a;
+      } else {
+        _a = parseInt(a, 10);
+      }
+    };
 
-		frameObj.getB = function() { return _b; };
+    frameObj.getB = function() { return _b; };
 
-		frameObj.setB = function(b) {
-			if(b == '/') {
-				_lookahead = 1;
-				_b = 10 - frameObj.getA();
-			} else if (b == '-') {
-				_b = 0;
-			} else if(b == undefined) {
-				_b = b;
-			} else {
-				_b = parseInt(b, 10);
-			}
-		};
+    frameObj.setB = function(b) {
+      if(b == '/') {
+        _lookahead = 1;
+        _b = 10 - frameObj.getA();
+      } else if (b == '-') {
+        _b = 0;
+      } else if(b == undefined) {
+        _b = b;
+      } else {
+        _b = parseInt(b, 10);
+      }
+    };
 
-		frameObj.getSubtotal = function() {
-			return (_a > -1 ? _a : 0) + (_b > -1 ? _b : 0);
-		};
+    frameObj.getSubtotal = function() {
+      return (_a > -1 ? _a : 0) + (_b > -1 ? _b : 0);
+    };
 
-		frameObj.getLookahead = function() {
-			return _lookahead;
-		};
+    frameObj.getLookahead = function() {
+      return _lookahead;
+    };
 
-		frameObj.canLookahead = function() {
-			return _lookahead > 0;
-		}
+    frameObj.canLookahead = function() {
+      return _lookahead > 0;
+    }
 
-		frameObj.decrementLookahead = function() {
-			_lookahead -= 1;
-		};
+    frameObj.decrementLookahead = function() {
+      _lookahead -= 1;
+    };
 
-		frameConstructor();
+    frameConstructor();
 
-		return frameObj;
-	};
+    return frameObj;
+  };
 
-	// For testing purposes
-	bowling.createFrame = function(a, b) {
-		return frame(a, b);
-	}
+  // For testing purposes
+  bowling.createFrame = function(a, b) {
+    return frame(a, b);
+  }
 
-	bowling.play = function(input) {
-		var frames = bowling.processGame(input);
+  bowling.play = function(input) {
+    var frames = bowling.processGame(input);
 
-		var gameTotal = bowling.processFrames(frames);
+    var gameTotal = bowling.processFrames(frames);
 
-		console.log(gameTotal);
-	};
+    console.log(gameTotal);
+  };
 
-	bowling.processGame = function(rawGame) {
-		return rawGame.split('').reduce(function(gameArray, current, index) {
-			var lastIndex = gameArray.length -1;
+  bowling.processGame = function(rawGame) {
+    return rawGame.split('').reduce(function(gameArray, current, index) {
+      var lastIndex = gameArray.length -1;
 
-			if(current == 'X') {
-				gameArray.push(frame(current, -1)); // Add Strike frame
-			} else if (current == '/') {
-				gameArray[lastIndex].setB(current); // Add Spare frame
-			} else if (gameArray.length == 0 || gameArray[lastIndex].getB() != undefined) {
-				gameArray.push(frame(current, undefined)); // Add regular frame
-			} else {
-				gameArray[lastIndex].setB(current); // This can only be 2nd bowl
-			}
-			return gameArray;
-		}, []);
-	};
+      if(current == 'X') {
+        gameArray.push(frame(current, -1)); // Add Strike frame
+      } else if (current == '/') {
+        gameArray[lastIndex].setB(current); // Add Spare frame
+      } else if (gameArray.length == 0 || gameArray[lastIndex].getB() != undefined) {
+        gameArray.push(frame(current, undefined)); // Add regular frame
+      } else {
+        gameArray[lastIndex].setB(current); // This can only be 2nd bowl
+      }
+      return gameArray;
+    }, []);
+  };
 
-	bowling.processFrames = function(frames) {
-		return frames.reduce(function(total, current, index, array) {
-			var lookaheadTotal = 0;
-			if(index < 10 && current.canLookahead()) {
-				array.slice(index + 1).every(function(currentLook) {
-					if(current.canLookahead() && currentLook.getA() >= 0) {
-						lookaheadTotal += currentLook.getA();
-						current.decrementLookahead();
-					}
+  bowling.processFrames = function(frames) {
+    return frames.reduce(function(total, current, index, array) {
+      var lookaheadTotal = 0;
+      if(index < 10 && current.canLookahead()) {
+        array.slice(index + 1).every(function(currentLook) {
+          if(current.canLookahead() && currentLook.getA() >= 0) {
+            lookaheadTotal += currentLook.getA();
+            current.decrementLookahead();
+          }
 
-					if(current.canLookahead() && currentLook.getB() >= 0) {
-						lookaheadTotal += currentLook.getB();
-						current.decrementLookahead();
-					}
-					return current.canLookahead();
-				});
-			}
-			total += lookaheadTotal + (index < 10 ? current.getSubtotal() : 0);
-			return total;
-		}, 0);
-	};
+          if(current.canLookahead() && currentLook.getB() >= 0) {
+            lookaheadTotal += currentLook.getB();
+            current.decrementLookahead();
+          }
+          return current.canLookahead();
+        });
+      }
+      total += lookaheadTotal + (index < 10 ? current.getSubtotal() : 0);
+      return total;
+    }, 0);
+  };
 
-	return bowling;
+  return bowling;
 }
 
 function main() {
-	var _input = "";
-	var game = bowlingGame();
+  var _input = "";
+  var game = bowlingGame();
 
-	function processData(input) {
-		game.play(input);
-	}
+  function processData(input) {
+    game.play(input);
+  }
 
-	process.stdin.setEncoding("utf8");
-	process.stdin.on("data", function (input) {
-	    _input += input;
-	});
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
 
-	process.stdin.on("end", function () {
-	   processData(_input);
-	});
+  process.stdin.on("end", function () {
+     processData(_input);
+  });
 }
 main();
--- mainSpec.js
+++ mainSpec.js
@@ -1,242 +1,242 @@
 describe('bowlingGame', function() {
-	var bowling = {};
-	beforeEach(function() {
-		bowling = bowlingGame();
-	});
+  var bowling = {};
+  beforeEach(function() {
+    bowling = bowlingGame();
+  });
 
-	describe('play', function() {
-		it('should process the game', function() {
-			spyOn(bowling, 'processGame').and.returnValue([1, 2, 3]);
-			spyOn(bowling, 'processFrames');
-			var input = 'X81';
+  describe('play', function() {
+    it('should process the game', function() {
+      spyOn(bowling, 'processGame').and.returnValue([1, 2, 3]);
+      spyOn(bowling, 'processFrames');
+      var input = 'X81';
 
-			bowling.play(input);
+      bowling.play(input);
 
-			expect(bowling.processGame).toHaveBeenCalledWith(input);
-			expect(bowling.processFrames).toHaveBeenCalledWith([1, 2, 3]);
-		});
-	});
+      expect(bowling.processGame).toHaveBeenCalledWith(input);
+      expect(bowling.processFrames).toHaveBeenCalledWith([1, 2, 3]);
+    });
+  });
 
-	describe('processGame', function() {
-		it('should return strike frame', function() {
-			var input = 'X';
-			var strikeFrame = bowling.processGame(input)[0];
+  describe('processGame', function() {
+    it('should return strike frame', function() {
+      var input = 'X';
+      var strikeFrame = bowling.processGame(input)[0];
 
-			expect(strikeFrame.getA()).toBe(10);
-			expect(strikeFrame.getB()).toBe(-1);
-			expect(strikeFrame.getLookahead()).toBe(2);
-		});
+      expect(strikeFrame.getA()).toBe(10);
+      expect(strikeFrame.getB()).toBe(-1);
+      expect(strikeFrame.getLookahead()).toBe(2);
+    });
 
-		it('should return spare frame', function() {
-			var input = '1/';
+    it('should return spare frame', function() {
+      var input = '1/';
 
-			var spareFrame = bowling.processGame(input)[0];
+      var spareFrame = bowling.processGame(input)[0];
 
-			expect(spareFrame.getA()).toBe(1);
-			expect(spareFrame.getB()).toBe(9);
-			expect(spareFrame.getLookahead()).toBe(1);
-		});
+      expect(spareFrame.getA()).toBe(1);
+      expect(spareFrame.getB()).toBe(9);
+      expect(spareFrame.getLookahead()).toBe(1);
+    });
 
-		it('should return regular frame', function() {
-			var input = '32';
+    it('should return regular frame', function() {
+      var input = '32';
 
-			var regularFrame = bowling.processGame(input)[0];
+      var regularFrame = bowling.processGame(input)[0];
 
-			expect(regularFrame.getA()).toBe(3);
-			expect(regularFrame.getB()).toBe(2);
-			expect(regularFrame.getLookahead()).toBe(0);
-		});
+      expect(regularFrame.getA()).toBe(3);
+      expect(regularFrame.getB()).toBe(2);
+      expect(regularFrame.getLookahead()).toBe(0);
+    });
 
-		it('should return 4 frames', function() {
-			var input = 'X81-92/';
+    it('should return 4 frames', function() {
+      var input = 'X81-92/';
 
-			expect(bowling.processGame(input).length).toBe(4);
-		});
-	});
+      expect(bowling.processGame(input).length).toBe(4);
+    });
+  });
 
-	describe('processFrames', function() {
-		it('should process strike lookahead', function() {
-			var input = 'X12----------------';
-			var frames = bowling.processGame(input);
+  describe('processFrames', function() {
+    it('should process strike lookahead', function() {
+      var input = 'X12----------------';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(16);
-		});
+      expect(bowling.processFrames(frames)).toBe(16);
+    });
 
-		it('should process spare lookahead', function() {
-			var input = '1/12----------------';
-			var frames = bowling.processGame(input);
+    it('should process spare lookahead', function() {
+      var input = '1/12----------------';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(14);
-		});
+      expect(bowling.processFrames(frames)).toBe(14);
+    });
 
-		it('should process multiple strikes', function() {
-			var input = 'XXXXXXXXXXXX';
-			var frames = bowling.processGame(input);
+    it('should process multiple strikes', function() {
+      var input = 'XXXXXXXXXXXX';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(300);
-		});
+      expect(bowling.processFrames(frames)).toBe(300);
+    });
 
-		it('should process game with no strikes or spares', function() {
-			var input = '9-9-9-9-9-9-9-9-9-9-';
-			var frames = bowling.processGame(input);
+    it('should process game with no strikes or spares', function() {
+      var input = '9-9-9-9-9-9-9-9-9-9-';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(90);
-		});
+      expect(bowling.processFrames(frames)).toBe(90);
+    });
 
-		it('should process game with only spares', function() {
-			var input = '5/5/5/5/5/5/5/5/5/5/5';
-			var frames = bowling.processGame(input);
+    it('should process game with only spares', function() {
+      var input = '5/5/5/5/5/5/5/5/5/5/5';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(150);
-		});
+      expect(bowling.processFrames(frames)).toBe(150);
+    });
 
-		it('should process game with a mix', function() {
-			var input = 'X7/9-X-88/-6XXX81';
-			var frames = bowling.processGame(input);
+    it('should process game with a mix', function() {
+      var input = 'X7/9-X-88/-6XXX81';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(167);
-		});
+      expect(bowling.processFrames(frames)).toBe(167);
+    });
 
-		it('should process game of a horrible bowler', function() {
-			var input = '--------------------';
-			var frames = bowling.processGame(input);
+    it('should process game of a horrible bowler', function() {
+      var input = '--------------------';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(0);
-		});
+      expect(bowling.processFrames(frames)).toBe(0);
+    });
 
-		it('should process horrible bowler who gets better at the end', function() {
-			var input = '--------------XXXXX';
-			var frames = bowling.processGame(input);
+    it('should process horrible bowler who gets better at the end', function() {
+      var input = '--------------XXXXX';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(90);
-		});
+      expect(bowling.processFrames(frames)).toBe(90);
+    });
 
-		it('should process unlucky bowler who gets worse at the end', function() {
-			var input = 'XXXXX--------------';
-			var frames = bowling.processGame(input);
+    it('should process unlucky bowler who gets worse at the end', function() {
+      var input = 'XXXXX--------------';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(120);
-		});
+      expect(bowling.processFrames(frames)).toBe(120);
+    });
 
-		it('should process horrible bowler who gets better over time', function() {
-			var input = '--12--3/22----XXXXX';
-			var frames = bowling.processGame(input);
+    it('should process horrible bowler who gets better over time', function() {
+      var input = '--12--3/22----XXXXX';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(109);
-		});
+      expect(bowling.processFrames(frames)).toBe(109);
+    });
 
-		it('should process unlucky bowler who gets worse over time', function() {
-			var input = 'XXXXX--5--7-91/9';
-			var frames = bowling.processGame(input);
+    it('should process unlucky bowler who gets worse over time', function() {
+      var input = 'XXXXX--5--7-91/9';
+      var frames = bowling.processGame(input);
 
-			expect(bowling.processFrames(frames)).toBe(160);
-		});
+      expect(bowling.processFrames(frames)).toBe(160);
+    });
 
-	});
+  });
 });
 
 describe('frame', function() {
-	var bowling = {};
-	var testFrame = {};
-	beforeEach(function() {
-		bowling = bowlingGame();
-	});
+  var bowling = {};
+  var testFrame = {};
+  beforeEach(function() {
+    bowling = bowlingGame();
+  });
 
-	describe('frameConstructor', function() {
-		it('should use default values', function() {
-			testFrame = bowling.createFrame(undefined, undefined);
+  describe('frameConstructor', function() {
+    it('should use default values', function() {
+      testFrame = bowling.createFrame(undefined, undefined);
 
-			expect(testFrame.getA()).toBe(undefined);
-			expect(testFrame.getB()).toBe(undefined);
-			expect(testFrame.getLookahead()).toBe(0);
-		});
-	});
+      expect(testFrame.getA()).toBe(undefined);
+      expect(testFrame.getB()).toBe(undefined);
+      expect(testFrame.getLookahead()).toBe(0);
+    });
+  });
 
-	describe('setA', function() {
-		beforeEach(function() {
-			testFrame = bowling.createFrame(undefined, undefined);
-		})
-		it('should parse X as 10 and set lookahead correctly', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+  describe('setA', function() {
+    beforeEach(function() {
+      testFrame = bowling.createFrame(undefined, undefined);
+    })
+    it('should parse X as 10 and set lookahead correctly', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setA('X');
+      testFrame.setA('X');
 
-			expect(testFrame.getA()).toBe(10);
-			expect(testFrame.getLookahead()).toBe(2);
-		});
+      expect(testFrame.getA()).toBe(10);
+      expect(testFrame.getLookahead()).toBe(2);
+    });
 
-		it('should parse - as 0 and leave lookahead untouched', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+    it('should parse - as 0 and leave lookahead untouched', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setA('-');
+      testFrame.setA('-');
 
-			expect(testFrame.getA()).toBe(0);
-			expect(testFrame.getLookahead()).toBe(0);
-		});
+      expect(testFrame.getA()).toBe(0);
+      expect(testFrame.getLookahead()).toBe(0);
+    });
 
-		it('should parse numeric strings as integers and leave lookahead untouched', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+    it('should parse numeric strings as integers and leave lookahead untouched', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setA('3');
+      testFrame.setA('3');
 
-			expect(testFrame.getA()).toBe(3);
-			expect(testFrame.getLookahead()).toBe(0);
-		});
-	});
+      expect(testFrame.getA()).toBe(3);
+      expect(testFrame.getLookahead()).toBe(0);
+    });
+  });
 
-	describe('setB', function() {
-		beforeEach(function() {
-			testFrame = bowling.createFrame(undefined, undefined);
-		});
+  describe('setB', function() {
+    beforeEach(function() {
+      testFrame = bowling.createFrame(undefined, undefined);
+    });
 
-		it('should parse / as remainder and set lookahead correctly', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+    it('should parse / as remainder and set lookahead correctly', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setA('4');
-			testFrame.setB('/');
+      testFrame.setA('4');
+      testFrame.setB('/');
 
-			expect(testFrame.getB()).toBe(6);
-			expect(testFrame.getLookahead()).toBe(1);
-		});
+      expect(testFrame.getB()).toBe(6);
+      expect(testFrame.getLookahead()).toBe(1);
+    });
 
-		it('should parse - as 0 and leave lookahead untouched', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+    it('should parse - as 0 and leave lookahead untouched', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setB('-');
+      testFrame.setB('-');
 
-			expect(testFrame.getB()).toBe(0);
-			expect(testFrame.getLookahead()).toBe(0);
-		})
+      expect(testFrame.getB()).toBe(0);
+      expect(testFrame.getLookahead()).toBe(0);
+    })
 
-		it('should parse numeric strings as integers and leave lookahead alone', function() {
-			expect(testFrame.getLookahead()).toBe(0);
+    it('should parse numeric strings as integers and leave lookahead alone', function() {
+      expect(testFrame.getLookahead()).toBe(0);
 
-			testFrame.setB('5');
+      testFrame.setB('5');
 
-			expect(testFrame.getB()).toBe(5);
-			expect(testFrame.getLookahead()).toBe(0);
-		});
-	});
+      expect(testFrame.getB()).toBe(5);
+      expect(testFrame.getLookahead()).toBe(0);
+    });
+  });
 
-	describe('getSubtotal', function() {
-		it('should return subtotal excluding of a and b', function() {
-			testFrame = bowling.createFrame(5, 3);
+  describe('getSubtotal', function() {
+    it('should return subtotal excluding of a and b', function() {
+      testFrame = bowling.createFrame(5, 3);
 
-			expect(testFrame.getSubtotal()).toBe(8);
-		});
+      expect(testFrame.getSubtotal()).toBe(8);
+    });
 
-		it('should return subtotal excluding negative values - a', function() {
-			testFrame = bowling.createFrame(-1, 1);
+    it('should return subtotal excluding negative values - a', function() {
+      testFrame = bowling.createFrame(-1, 1);
 
-			expect(testFrame.getSubtotal()).toBe(1);
-		});
+      expect(testFrame.getSubtotal()).toBe(1);
+    });
 
-		it('should return subtotal excluding negative values - b', function() {
-			testFrame = bowling.createFrame(1, -1);
+    it('should return subtotal excluding negative values - b', function() {
+      testFrame = bowling.createFrame(1, -1);
 
-			expect(testFrame.getSubtotal()).toBe(1);
-		});
-	});
+      expect(testFrame.getSubtotal()).toBe(1);
+    });
+  });
 });
 
 
